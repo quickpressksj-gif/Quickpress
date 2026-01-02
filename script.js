f7357acfbd652ba3cadc0f6a252b456b3773bd24
quickpressksj-gif <quickpressksj@gmail.com>
2026-01-02 14:31:27 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,3 +5,22 @@ function openPanel() {
 function closePanel() {
   document.getElementById("orderPanel").classList.remove("open");
 }
+function openPanel(){
+  document.getElementById("orderPanel").classList.add("open");
+}
+
+function closePanel(){
+  document.getElementById("orderPanel").classList.remove("open");
+}
+
+function goToDelivery(){
+  document.getElementById("pickupStep").style.display="none";
+  document.getElementById("deliveryStep").style.display="block";
+  document.getElementById("panelTitle").innerText="Delivery Details";
+}
+
+function confirmOrder(){
+  document.getElementById("deliveryStep").style.display="none";
+  document.getElementById("thankYouStep").style.display="block";
+  document.getElementById("panelTitle").innerText="Thank You";
+}

9fbdb618b1854266bb6a0df0803c582297e63268
quickpressksj-gif <quickpressksj@gmail.com>
2026-01-02 14:36:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -5,22 +5,15 @@ function openPanel() {
 function closePanel() {
   document.getElementById("orderPanel").classList.remove("open");
 }
-function openPanel(){
-  document.getElementById("orderPanel").classList.add("open");
+
+function goToDelivery() {
+  document.getElementById("pickupStep").style.display = "none";
+  document.getElementById("deliveryStep").style.display = "block";
+  document.getElementById("panelTitle").innerText = "Delivery Details";
 }
 
-function closePanel(){
-  document.getElementById("orderPanel").classList.remove("open");
-}
-
-function goToDelivery(){
-  document.getElementById("pickupStep").style.display="none";
-  document.getElementById("deliveryStep").style.display="block";
-  document.getElementById("panelTitle").innerText="Delivery Details";
-}
-
-function confirmOrder(){
-  document.getElementById("deliveryStep").style.display="none";
-  document.getElementById("thankYouStep").style.display="block";
-  document.getElementById("panelTitle").innerText="Thank You";
+function confirmOrder() {
+  document.getElementById("deliveryStep").style.display = "none";
+  document.getElementById("thankYouStep").style.display = "block";
+  document.getElementById("panelTitle").innerText = "Thank You";
 }
